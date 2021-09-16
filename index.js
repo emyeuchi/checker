d82da00f376da34bc326c5c9aa949117cdf7199b
emyeuchi <62176715+emyeuchi@users.noreply.github.com>
2021-09-16 13:17:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 //ボタンを押すごとに画面が切り替わる関数
   $(function () {
+
     $(".btn").on("click", function () {
       $(this).closest("div").css("display", "none");
       id = $(this).attr("href");
       $(id).addClass("fit").fadeIn("slow").show();
     });
-      
-      //選択ボタンデータを配列に入れてカウントする関数
+
+
+
+//選択ボタンデータを配列に入れてカウントする関数
       var countA;
       var countB;
       var box =[];
@@ -14,6 +17,7 @@
       $(this).on('click',function(){
         var value = $(this).data("value");
          box.push(value);
+
         countA = box.filter(function(x){
                       return x === "a"
                     }).length;
@@ -22,11 +26,15 @@
                     }).length;
       });
     });
-      //結果を出力する関数
+
+
+//結果を出力する関数
     $('.end').on('click',function(){
       if( countA > countB ) {
         $('#answer_01').css("display",""); //回答1
       } else {
         $('#answer_02').css("display","");//回答2
     }
+  });
+
   });
