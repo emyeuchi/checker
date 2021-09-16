7d736bf6dbd401b65b293258207d541e329c14c1
emyeuchi <62176715+emyeuchi@users.noreply.github.com>
2021-09-16 12:27:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,30 +2,31 @@ function check() {
     let str = [];
     //ラジオボタンの中身を取得
     const q = [document.question.q0, document.question.q1, document.question.q2];
-    //A・B・Cそれぞれの合計点数を入れるための変数
-    var yy = 0;
-    var nn = 0;
+  
+  //A・B・Cそれぞれの合計点数を入れるための変数
+    var aaa = 0;
+    var bbb = 0;
     //どのラジオボタンが選択されたか判定し、点数を加算
     for (let n = 0; n < q.length; n++) {
         for (let i = 0; i < q[n].length; i++) {
             if (q[n][i].checked) {
                 str[n] = q[n][i].value;
-                if (str[n] == "N") {
-                    yy++;
-                } else if (str[n] == "Y") {
-                    nn++;
-                }
+                if (str[n] == "A") {
+                    aaa++;
+                } else if (str[n] == "B") {
+                    bbb++;
+                } 
                 break;
             }
         }
     }
     //コメントの条件分岐
     var message = "";
-    if (nn = 3) {
+    if (bbb = 3) {
         message = "塑史向いてないよ";
-    } else if (nn = 2) {
+    } else if (bbb = 2) {
         message = "頑張ろう";
-    } else if (nn = 1) {
+    } else if (bbb = 1) {
         message = "「なかなか」やるな";
     } else {
         message = "あなたは塑史合格です！ 死んだ方がいいよ";
